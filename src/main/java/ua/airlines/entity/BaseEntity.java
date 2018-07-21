d7d0808e81e554f66af821cf12fe9b23b64f4e4e
EksTRIM <myxrom@gmail.com>
2018-07-21 11:04:25 +0300

    eee

---
+++ src/main/java/ua/airlines/entity/BaseEntity.java
@@ -0,0 +1,12 @@
+package ua.airlines.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+}
