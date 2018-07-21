7fd37ca29de5ce82609a4904bcbe218346990a42
EksTRIM <myxrom@gmail.com>
2018-07-21 11:11:08 +0300

    Add company

---
+++ src/main/java/ua/airlines/entity/Air_company.java
@@ -0,0 +1,26 @@
+package ua.airlines.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "air_company")
+public class Air_company extends BaseEntity{
+
+    @Column(name = "compan_name",length = 150,nullable = false)
+    private String companyName;
+
+    @Column(name = "company_address", nullable = false)
+    private String companyAddress;
+
+}
